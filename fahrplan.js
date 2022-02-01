d36f0f22cfd4f5a926473626902253308e463a37
Lukas1803 <lukasgallmetzer1@gmail.com>
2022-02-01 13:06:04 +0100

    Update fahrplan.js

--- fahrplan.js
+++ fahrplan.js
@@ -1,3 +1,54 @@
+function display_c7(){
+    var refresh=1000; // Refresh rate in milli seconds
+    mytime=setTimeout('display_ct7()',refresh);
+}
+
+function aufrufFahrplan(){
+    mytime = setTimeout('einlesen()', 10000);
+}
+
+function einlesen(){
+    //console.log("Test");
+    fetch("https://efa.sta.bz.it/apb/XML_DM_REQUEST?&locationServerActive=1&stateless=1&type_dm=any&name_dm=Brixen%20Brixen%20Dantestra%C3%9Fe&mode=direct&outputFormat=json")
+        .then(response => {
+            if (response.ok) {
+                return response.json();
+            } else {
+                aufrufFahrplan();
+                //throw new Error('Something went wrong');
+            }
+        })
+        .then(json => kontrolle(json));
+}
+
+function kontrolle(data){
+
+    let linieId = "";
+    let zielId = "";
+    let abfahrtId = "";
+    let minu = 0;
+    let stunde = 0;
+    let dauer = 0;
+
+    for(let i=0; i<14; i++){
+        //console.log(data.departureList[i].servingLine.direction);
+        linieId = "linie" + i;
+        zielId = "ziel" + i;
+        abfahrtId = "abfahrt" + i;
+        stunde = data.departureList[i].dateTime.hour;
+        minu = data.departureList[i].dateTime.minute;
+        
+
+        document.getElementById(linieId).innerHTML = data.departureList[i].servingLine.number;
+        document.getElementById(zielId).innerHTML = data.departureList[i].servingLine.direction;
+        
+        if (minu<10) document.getElementById(abfahrtId).innerHTML = stunde + ":0" + minu;
+        else document.getElementById(abfahrtId).innerHTML = stunde + ":" + minu;
+    
+        aufrufFahrplan();
+    }
+}
+
 function display_ct7(){
     var x = new Date()
     var ampm = x.getHours( ) >= 12 ? '' : '';
@@ -15,10 +66,7 @@ function display_ct7(){
     x1 = x1 + " - " +  hours + ":" +  minutes + ":" +  seconds + " " + ampm;
     document.getElementById('zeit').innerHTML = x1;
     display_c7();
- }
-
- function display_c7(){
-    var refresh=1000; // Refresh rate in milli seconds
-    mytime=setTimeout('display_ct7()',refresh)
 }
-display_c7()
+
+einlesen();
+display_c7();
