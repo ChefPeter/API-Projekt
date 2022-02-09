68b1888ca30e0ad881c8e61fb6bcd5ba37264ca0
Peter Costadoi <74654354+ChefPeter@users.noreply.github.com>
2022-02-09 11:13:00 +0100

    sers

--- fahrplan.js
+++ fahrplan.js
@@ -84,12 +84,12 @@ function wetter(data){
     document.getElementById("wind").innerHTML = "&nbsp;" + data.wind.speed + "m/s";
     document.getElementById("wetterart").innerHTML = data.weather[0].description;
 
-    document.getElementById("allgemein1").src = bild;
+    document.getElementById("allgemeinMg").src = bild;
     document.getElementsByClassName("wetterMg")[0].style.backgroundImage = hintergrund;
     
-    document.getElementById("nieder1").innerHTML = "&nbsp;" + data.main.humidity + "% Luftfeuchtigkeit";
-    document.getElementById("wind1").innerHTML = "&nbsp;" + data.wind.speed + "m/s";
-    document.getElementById("wetterart1").innerHTML = data.weather[0].description;
+    document.getElementById("niederMg").innerHTML = "&nbsp;" + data.main.humidity + "% Luftfeuchtigkeit";
+    document.getElementById("windMg").innerHTML = "&nbsp;" + data.wind.speed + "m/s";
+    document.getElementById("wetterartMg").innerHTML = data.weather[0].description;
 
     aufrufWetter();
 }
